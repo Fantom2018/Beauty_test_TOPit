fd4f8773c979c96673b997012d4a1e9e56b6af82
Faizulla <fant@zzz.xyz>
2022-02-11 02:31:29 +0500

    Login Screen in progress

---
+++ lib/routs/app_routes.dart
@@ -0,0 +1,8 @@
+part of 'app_pages.dart';
+
+abstract class Routes {
+  // Main Menu Route
+  static const WELCOME = '/master_profile_screen';
+  static const LOGIN = '/login_lk_screen';
+  //static const ABOUT = '/about-you';
+}
